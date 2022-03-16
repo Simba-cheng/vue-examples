3684304db6f3967f0028b2a3e45f19738c6ecab6
yxcheng <yixincheng@pateo.com.cn>
2022-03-16 16:11:22 +0800

    refactor(example): update

---
+++ src/TestApp.vue
@@ -0,0 +1,25 @@
+<template>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld2 msg="哈哈哈，你在说啥？"></HelloWorld2>
+</template>
+
+<script>
+    import HelloWorld2 from "./components/HelloWorld2.vue";
+    export default {
+        name: 'testApp',
+        components: {
+            HelloWorld2
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
