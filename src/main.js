5ebac40622ec67fd5c88f67a9db5d6c5e209d1bd
yxcheng <yixincheng@pateo.com.cn>
2022-03-17 15:50:31 +0800

    refactor(examples): update

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@
 
 
 import {createApp} from "vue";
-import testApp from "./TestApp.vue";
 
-createApp(testApp).mount('#app')
+//import componentAppTest01 from "./ComponentAppTest01.vue";
+import componentAppTest02 from "@/components/ComponentAppTest02.vue";
+
+//createApp(componentAppTest01).mount('#app')
+createApp(componentAppTest02).mount('#app')
