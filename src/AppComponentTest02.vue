a6c2a2e473248c5f19ffe7f0ebc8880385b1bed2
yxcheng <yixincheng@pateo.com.cn>
2022-03-17 16:34:06 +0800

    refactor(examples): update

--- src/AppComponentTest02.vue
+++ src/AppComponentTest02.vue
@@ -2,19 +2,27 @@
     <div>
         <date-picker @run-parent="getChild"></date-picker>
     </div>
+
+    <p></p>
+
+    <div>
+        <custom-components01 @submit="getChild"></custom-components01>
+    </div>
 </template>
 
 <script>
     import datePicker from "@/components/childComponents02/DatePicker.vue";
+    import customComponents01 from "@/components/childComponents02/customComponents01.vue";
 
     export default {
         name: "appComponentTest02",
         components: {
-            datePicker
+            datePicker,
+            customComponents01
         },
         methods: {
             getChild(data) {
-                console.log("data: " + data)
+                console.log("data -- :" + data.username + " " + data.password)
             },
         }
     }
