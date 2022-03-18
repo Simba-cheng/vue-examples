74e13e405692a5b5193fa8900b8f0ae2c7f2ccc6
yxcheng <yixincheng@pateo.com.cn>
2022-03-18 10:34:09 +0800

    refactor(examples): update

--- src/components/childComponents02/DynamicComponent.vue
+++ src/components/childComponents02/DynamicComponent.vue
@@ -1,17 +1,35 @@
 <template>
     <div id="app">
+
+        <!--放置tab点击标签-->
         <div class="top">
-            <!--放置tab点击标签-->
-            <div class="cardClass" :class="{highLight:whichIndex === index}" v-for="(item,index) in cardArr"
-                 :key="index" @click="whichIndex = index;componentId = item.componentId">
+
+            <!--
+                for循环cardArr,在data中定义的点击的tab数据
+            -->
+            <div class="cardClass" :class="{highLight:whichIndex === index}"
+                 v-for="(item,index) in cardArr" :key="index"
+                 @click="whichIndex = index;componentId = item.componentId">
                 {{ item.componentName }}
             </div>
         </div>
+
+        <!--动态组件呈现对应内容-->
         <div class="bottom">
-            <!--动态组件呈现对应内容-->
-            <keep-alive>
+
+            <!--
+                动态组件标签：component。
+                该标签的is属性,is值为谁，就渲染谁。
+            -->
+            <component :is="componentId"></component>
+
+            <!--
+                keep-alive 缓存组件
+                组件不会被销毁，dom不会被重新渲染，浏览器不会回流、重绘，优化性能。
+            -->
+            <!--<keep-alive>
                 <component :is="componentId"></component>
-            </keep-alive>
+            </keep-alive>-->
 
         </div>
     </div>
@@ -21,25 +39,34 @@
     https://juejin.cn/post/6957696034449391646
 -->
 <script>
+
+    // 引入定义好的tab组件
     import helloTab from "@/components/childComponents02/tabs/HelloTab.vue";
     import postsTab from "@/components/childComponents02/tabs/PostsTab.vue";
     import archiveTab from "@/components/childComponents02/tabs/ArchiveTab.vue";
 
     export default {
         name: "dynamicComponent",
+
+        // 注册tab组件
         components: {
             helloTab,
             postsTab,
             archiveTab
         },
+
         data() {
             return {
                 whichIndex: 0,
+
+                //默认展示标签
                 componentId: "helloTab",
+
+                //定义存放点击的tab数据
                 cardArr: [
-                    {componentName: "组件一", componentId: "helloTab"},
-                    {componentName: "组件二", componentId: "postsTab"},
-                    {componentName: "组件三", componentId: "archiveTab"}
+                    {componentName: "helloTab", componentId: "helloTab"},
+                    {componentName: "postsTab", componentId: "postsTab"},
+                    {componentName: "archiveTab", componentId: "archiveTab"}
                 ]
             }
         },
