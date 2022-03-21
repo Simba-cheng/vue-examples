81f6fa06013ab3315a51520928031b6dd76e4d37
yxcheng <yixincheng@pateo.com.cn>
2022-03-21 14:12:23 +0800

    refactor(examples): update

--- packages/routers/02-router/src/App.vue
+++ packages/routers/02-router/src/App.vue
@@ -3,12 +3,21 @@
 
     <div>
         <h1>Hello Router</h1>
+
+        <!-- router-link标签的to属性，对应一条路由 -->
         <p>
             <router-link to="/">go to home</router-link>
             <br/>
-            <br/>
             <router-link to="/about">go to about</router-link>
+            <br/>
+            <router-link to="/users/1">go to user1</router-link>
+            <br/>
+            <router-link to="/users/2">go to user2</router-link>
+            <br/>
+            <router-link to="/users/3">go to user3</router-link>
         </p>
+
+        <!--用于放置路由映射对应的页面-->
         <router-view></router-view>
     </div>
 
---
+++ packages/routers/02-router/src/components/User.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>It's User Page</div>
+        <br>
+        <div>id是 {{ this.$route.params.id }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "user-page"
+    }
+</script>
+
+<style scoped>
+
+</style>
