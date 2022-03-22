3460927abc634bec7d209b379b59b48b7add3cf7
yxcheng <yixincheng@pateo.com.cn>
2022-03-22 13:58:57 +0800

    refactor(examples): update

---
+++ packages/component-watch/01-watch/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <container></container>
+</template>
+
+<script>
+
+    import container from "@/components/Container.vue";
+
+    export default {
+        name: 'App',
+        components: {
+            container
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
