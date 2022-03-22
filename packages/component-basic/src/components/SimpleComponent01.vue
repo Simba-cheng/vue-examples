09231f65834a5262f1abb9d1e48aea34b0032fe8
yxcheng <yixincheng@pateo.com.cn>
2022-03-22 14:17:44 +0800

    refactor(examples): update

--- packages/component-basic/src/components/SimpleComponent01.vue
+++ packages/component-basic/src/components/SimpleComponent01.vue
@@ -2,16 +2,45 @@
     <div>
         <p class="greeting"> {{ greeting }}</p>
     </div>
+    <div>
+        {{ testData }}
+    </div>
+    <button @click="addTestDataClick">累加testData</button>
+    <br>
+    <button @click="printTestDataClick">输出testData</button>
 </template>
 
 <script>
+    import {ref} from "vue";
+
+    const count = ref(0);
+    console.log('count:', count.value)
+    count.value++
+    console.log('count:', count.value)
+
     export default {
         name: 'simpleComponent01',
+
+        setup() {
+            return {
+                testData: ref(0)
+            }
+        },
+
         data() {
             return {
                 greeting: '你好？我不好'
             }
         },
+
+        methods: {
+            addTestDataClick() {
+                this.testData += 1;
+            },
+            printTestDataClick() {
+                console.info('testData:' + this.testData);
+            },
+        }
     }
 </script>
 
