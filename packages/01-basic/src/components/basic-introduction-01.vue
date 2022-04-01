01cfcc66eba6ebe2f1d5ba7e4135ea2138fb0cbb
yxcheng <yixincheng@pateo.com.cn>
2022-04-01 17:46:27 +0800

    refactor(components): update

---
+++ packages/01-basic/src/components/basic-introduction-01.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <h1>{{ message }}</h1>
+
+</template>
+
+<script>
+  export default {
+    name: 'basic-introduction-01',
+    data() {
+      return {
+        message: '我是message'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
