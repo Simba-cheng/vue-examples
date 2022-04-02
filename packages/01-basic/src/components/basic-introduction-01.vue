c88bbbb134de8b321622ac42d09ea82892acdc5f
yxcheng <yixincheng@pateo.com.cn>
2022-04-02 09:34:08 +0800

    refactor(components): update

--- packages/01-basic/src/components/basic-introduction-01.vue
+++ packages/01-basic/src/components/basic-introduction-01.vue
@@ -1,6 +1,19 @@
 <template>
 
-  <h1>{{ message }}</h1>
+  <!--声明式渲染-->
+  <h5>{{ message }}</h5>
+
+  <!--字符串反转-->
+  <button @click="reverseMessage">Reverse Message</button>
+  <br>
+  <!--字符串追加-->
+  <button @click="message += '!'">追加字符串(写法一)</button>
+  <br>
+  <button @click="messageAppend">追加字符串(写法二)</button>
+  <br>
+
+  <!--点击弹窗-->
+  <span @click.prevent="notify">点击弹窗</span>
 
 </template>
 
@@ -10,6 +23,17 @@
     data() {
       return {
         message: '我是message'
+      }
+    },
+    methods: {
+      reverseMessage() {
+        this.message = this.message.split('').reverse().join('')
+      },
+      messageAppend() {
+        this.message += '~'
+      },
+      notify() {
+        alert('哈哈哈哈，你是谁？')
       }
     }
   }
