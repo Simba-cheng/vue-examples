b944c3019e1463c3eab0d59bcf7cceeb3f33dd9e
yxcheng <yixincheng@pateo.com.cn>
2022-04-02 13:58:44 +0800

    chore(submodule): update

---
+++ packages/01-basic/src/components/Router-01.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'Router-01'
+  }
+</script>
+
+<style scoped>
+
+</style>
--- packages/01-basic/src/components/computed-01.vue
+++ packages/01-basic/src/components/computed-01.vue
@@ -1,16 +1,22 @@
 <template>
-  <div id="computed-01">
+  <div id="01">
     <span>是否有出版书籍：</span>
     <!--<span>{{ author.books.length > 0 ? 'yes' : 'no' }}</span>-->
-    <span>{{publishedBooksMessage}}</span>
+    <span>{{ publishedBooksMessage }}</span>
 
     <ul v-if="author.books.length > 0">
       <li v-for="book in author.books" :key="book">{{ book }}</li>
     </ul>
     <br>
     <button @click="author.books.pop()">删除一本书</button>
-
   </div>
+
+  <div id="02">
+    姓：<input type="text" placeholder="first name" v-model="firstName"><br>
+    名：<input type="text" placeholder="last name" v-model="lastName"><br>
+    全名<input type="text" placeholder="fullName" v-model="fullName"><br>
+  </div>
+
 </template>
 
 <script>
@@ -25,13 +31,21 @@
             'Vue 3 - Basic Guide',
             'Vue 4 - The Mystery'
           ]
-        }
+        },
+        firstName: '',
+        lastName: ''
       }
     },
     computed: {
       publishedBooksMessage() {
         return this.author.books.length > 0 ? 'yes' : 'no'
+      },
+      fullName() {
+        return this.firstName + ' ' + this.lastName
       }
+      //      fullName: function() {
+      //        return this.firstName + ' ' + this.lastName
+      //      }
     }
   }
 </script>
