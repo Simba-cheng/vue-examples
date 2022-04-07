7369cc1d49661a0ac7722aeed3eafb22718ab035
yxcheng <yixincheng@pateo.com.cn>
2022-04-07 16:01:49 +0800

    update

--- packages/01-container/src/components/commonLayout03.vue
+++ packages/01-container/src/components/commonLayout03.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="commonLayout03">
-    <el-container style="height: 1000px; border: 1px solid #eee">
-      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+    <el-container style="height: 100%; border: 1px solid #eee">
+      <el-aside width="15%" style="background-color: rgb(238, 241, 246)">
         <el-menu :default-openeds="['1','3']">
           <el-sub-menu index="1">
             <template #title>
@@ -109,10 +109,11 @@
 .el-header {
   background-color: #b3c0d1;
   color: var(--el-text-color-primary);
-  line-height: 60px;
+  line-height: 100%;
 }
 
 .el-aside {
+  height: 100%;
   color: var(--el-text-color-primary);
 }
 
